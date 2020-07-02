b5819612bbd3966745ffa7e7612809093b6cc62b
Alexandre Gélinas <60028306+LEX0RE@users.noreply.github.com>
2020-07-02 17:59:44 -0400

    Add files via upload

---
+++ Sources/cmake-build-debug/CMakeFiles/PI2020.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Git-PI2020/Programme/PI2020/Sources/main.cpp" "D:/Git-PI2020/Programme/PI2020/Sources/cmake-build-debug/CMakeFiles/PI2020.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Optional"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Sources/cmake-build-debug/CMakeFiles/PI2020.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PI2020.dir/main.cpp.obj"
+  "PI2020.exe"
+  "PI2020.exe.manifest"
+  "PI2020.pdb"
+  "libPI2020.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PI2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
